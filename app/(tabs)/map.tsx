0c1f0d89864b81b420fcbb021238598fd523b02a
pedroveg <113405018+pedroveg@users.noreply.github.com>
2025-09-28 14:52:17 -0600

    [28/9/25] Implementation of part of the map module with a test of the San José hospital location.

---
+++ app/(tabs)/map.tsx
@@ -0,0 +1,20 @@
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet } from 'react-native';
+
+export default function MapScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Función no disponible</ThemedText>
+      <ThemedText>El mapa de recursos solo está disponible en la aplicación móvil.</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
